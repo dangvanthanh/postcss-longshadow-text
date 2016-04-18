2a79f0f3a04280c9c62cf4e6e2de721b2ba5458a
Dang Van Thanh <dangvanthanh@dangthanh.org>
2016-04-18 12:55:12 +0700

    Initialize

---
+++ index.js
@@ -0,0 +1,45 @@
+var postcss = require('postcss');
+
+var pluginName = 'postcss-longshadow-text';
+
+module.exports = postcss.plugin(pluginName, function (opts) {
+	opts = opts || {};
+
+	return function (css) {
+		css.walkDecls('longshadow-text', function (decl) {
+			var sizes = postcss.list.space(decl.value);
+			var cssDirection = sizes[0];
+			var cssSize = parseInt(sizes[1], 10);
+			var cssColor = sizes[2];
+			var cssTextshadow = '';
+
+			for (var i = 0; i < cssSize; i++) {
+				if (cssDirection === 'top') {
+					cssTextshadow += '0 ' + -i + 'px 0 ' + cssColor + ',';
+				} else if (cssDirection === 'right') {
+					cssTextshadow += i + 'px 0 0 ' + cssColor + ',';
+				} else if (cssDirection === 'bottom') {
+					cssTextshadow += '0 ' + i + 'px 0 ' + cssColor + ',';
+				} else if (cssDirection === 'left') {
+					cssTextshadow += -i + 'px 0 0 ' + cssColor + ',';
+				} else if (cssDirection === 'top-left') {
+					cssTextshadow += -i + 'px ' + -i + 'px 0 ' + cssColor + ',';
+				} else if (cssDirection === 'top-right') {
+					cssTextshadow += i + 'px ' + i + 'px 0 ' + cssColor + ',';
+				} else if (cssDirection === 'bottom-left') {
+					cssTextshadow += -i + 'px ' + i + 'px 0 ' + cssColor + ',';
+				} else if (cssDirection === 'bottom-right') {
+					cssTextshadow += i + 'px ' + i + 'px 0 ' + cssColor + ',';
+				}
+			}
+
+			cssTextshadow = cssTextshadow.slice(0, -1);
+
+			decl.cloneBefore({
+				prop: 'text-shadow', value: cssTextshadow
+			});
+
+			decl.remove();
+		});
+	};
+});
---
+++ readme.md
@@ -0,0 +1,51 @@
+# postcss-longshadow-text
+
+[![Build Status](https://travis-ci.org/dangvanthanh/postcss-longshadow-text.svg?branch=master)](https://travis-ci.org/dangvanthanh/postcss-longshadow-text)
+
+> Postcss plugin for long shadow text
+
+
+## Install
+
+```
+$ npm install [--save] postcss-longshadow-text
+```
+
+
+## Configure
+
+```js
+postcss([require('postcss-tipsy')])
+```
+
+See [Postcss](https://github.com/postcss/postcss) docs for examples for your environment.
+
+## API
+
+### Stylesheets
+
+```css
+.foo {
+	longshadow-text: [direction] [size] [color];
+}
+```
+
+Examples
+
+```
+.foo {
+	longshadow-text: top 70 #a13c32;
+}
+```
+
+### Options
+
+| Options    | Description                 | Value                                                                   |
+|------------|-----------------------------|-------------------------------------------------------------------------|
+| direction  | Position of shadow text     | top, right, bottom, left, top-left, top-right, bttom-left, bottom-right |
+| size       | Size of shadow text         | Number such as 10, 20, 30                                               |
+| color      | Color of shadow text        | HEX, RGB, RGBA, HSL                                                     |
+
+## License
+
+MIT © [Dang Van Thanh](http://dangthanh.org)
